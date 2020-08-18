1099f17d645908904cbf7610fd5a131dd156e5dc
nam <vannam9a3@gmail.com>
2020-08-19 07:21:40 +0900

    Updates

---
+++ precache-manifest.9a109ee9057865ca074aa8cebd60c9e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68ee4056c8656c517c2694d49dcde565",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "b93e9302ef51f49a5a1f",
+    "url": "/pomodoro-clock/static/css/main.b4e2751e.chunk.css"
+  },
+  {
+    "revision": "17a1dad7a2cedc692d88",
+    "url": "/pomodoro-clock/static/js/2.fe125dfc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro-clock/static/js/2.fe125dfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b93e9302ef51f49a5a1f",
+    "url": "/pomodoro-clock/static/js/main.983ecb33.chunk.js"
+  },
+  {
+    "revision": "d420e891230cef74bfe7",
+    "url": "/pomodoro-clock/static/js/runtime-main.cd73fa2f.js"
+  }
+]);
